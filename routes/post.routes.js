1df2a89591662160c097bf446fbf12a95b9d6bb4
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-07-22 07:56:32 +0200

    Recap #02

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -4,3 +4,8 @@ const Post = require('../models/Post.model');
 const fileUploader = require('../config/cloudinary.config');
 
 // GET route ==> to display the post form
+router.get('/posts/new',isLoggedIn,(req,res) => {
+    res.render('posts/new-post');
+});
+
+module.exports = router;
